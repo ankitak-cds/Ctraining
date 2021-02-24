2f0d9e5f77a195bd3fe9f4eb52b599d3ca428e6b
ankitak-cds <79127556+ankitak-cds@users.noreply.github.com>
2021-02-24 12:19:40 +0530

    Update pcComm.c

--- pcComm.c
+++ pcComm.c
@@ -1,44 +1,18 @@
 #include <stdio.h>
-#include<stdlib.h> 
-#include<unistd.h>
+#include <unistd.h>
+#define MSGSIZE 16
+char* msg1 = "hello";
 int main()
 {
-    pid_t pid;
-    int r;
-    char *ch=NULL;
-    char *ch1=NULL;
-    int readpipe[2];
-    int writepipe[2];
-    int a;
-    int b;
-    a=pipe(readpipe);
-    b=pipe(writepipe);
-      // check a and b
-
-    pid=fork();
-    // check pid
-
-    if(pid==0)
-    { //CHILD PROCESS
-            close(readpipe[1]);
-            close(writepipe[1]);
-            read(readpipe[0],ch,sizeof(ch));
-            printf("\nREAD = %s",ch);
-            close(readpipe[0]);
-            ch1="YES";
-            write(writepipe[1],ch1,sizeof(ch1));
-            close(writepipe[1]);
-    }
-    else
-    { //PARENT PROCESS
-            close(writepipe[0]);
-            close(writepipe[1]);
-            ch="HI!! YOU THERE";
-            write(readpipe[1],ch,sizeof(ch));
-            close(readpipe[1]);
-            read(writepipe[1],ch1,sizeof(ch1));
-            printf("\nACK RECEIVED %s",ch1);
-            close(writepipe[1]);
+    char buf[MSGSIZE];
+    int p[2],i;
+    if(pipe(p) < 0)
+        exit(1);
+   
+    write(p[1],msg,MSGSIZE);
+   
+    read(p[0],buf,MSGSIZE);
+    printf("%s\n",buf);
     }
     return 0;
 }
